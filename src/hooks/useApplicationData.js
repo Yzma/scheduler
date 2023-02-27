14b89661ca0b216650f454de7db497c97b36594a
Yzma <10090913+Yzma@users.noreply.github.com>
2023-02-27 14:16:22 -0500

    Move updateSpots method and start work on sockets

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -7,13 +7,13 @@ const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 const SET_INTERVIEW = "SET_INTERVIEW";
 
 const reducer = (state, action) => {
-  console.log('ACTION: ', action)
   switch (action.type) {
     case SET_DAY:
       return {
         ...state,
         day: action.value
       }
+
     case SET_APPLICATION_DATA:
       return {
         ...state,
@@ -23,10 +23,12 @@ const reducer = (state, action) => {
       }
 
     case SET_INTERVIEW:
+      console.log('reducer appointments: ', action.value.appointments)
+
       return {
         ...state,
         appointments: action.value.appointments,
-        days: action.value.days
+        days: updateSpots(state, action.value.appointments)
       }
 
     default:
@@ -34,6 +36,20 @@ const reducer = (state, action) => {
         `Tried to reduce with unsupported action type: ${action.type}`
       );
   }
+}
+
+const updateSpots = (state, appointments) => {
+  return state.days.map(stateDay => {
+    if (stateDay.name !== state.day) {
+      return stateDay
+    }
+
+    const spotsRemaining = stateDay.appointments.filter(e => appointments[e].interview === null).length
+    return {
+      ...stateDay,
+      spots: spotsRemaining
+    }
+  })
 }
 
 const useApplicationData = () => {
@@ -45,14 +61,38 @@ const useApplicationData = () => {
   });
 
   useEffect(() => {
+    const socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+    socket.onopen = (event) => {
+      socket.send("ping");
+    };
+
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+
+      console.log("Client Message Received:", data);
+
+      if(data.type === "SET_INTERVIEW") {
+
+        console.log('copy copyTest: ', state)
+
+        //dispatch({ type: SET_INTERVIEW, value: { appointments } })
+      }
+    }
+
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
       axios.get("http://localhost:8001/api/appointments"),
       axios.get("http://localhost:8001/api/interviewers")
     ]).then((all) => {
       console.log('All: ', all)
-      dispatch({ type: "SET_APPLICATION_DATA", value: { days: all[0].data, appointments: all[1].data, interviewers: all[2].data } })
+      dispatch({ type: SET_APPLICATION_DATA, value: { days: all[0].data, appointments: all[1].data, interviewers: all[2].data } })
     });
+
+    return () => {
+      socket.close()
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const bookInterview = (id, interview) => {
@@ -67,11 +107,10 @@ const useApplicationData = () => {
     };
 
     console.log(id, interview);
-    const updatedDaysCopy = updateSpots(appointments)
 
     return axios.put(`http://localhost:8001/api/appointments/${id}`, { id, interview })
       .then(() => {
-        dispatch({ type: SET_INTERVIEW, value: { appointments, days: updatedDaysCopy } })
+        dispatch({ type: SET_INTERVIEW, value: { appointments } })
         console.log('Updated state successfully')
       })
   }
@@ -87,27 +126,11 @@ const useApplicationData = () => {
       [id]: appointment
     };
 
-    const updatedDaysCopy = updateSpots(appointments)
-
     return axios.delete(`http://localhost:8001/api/appointments/${id}`)
       .then(() => {
-        dispatch({ type: SET_INTERVIEW, value: { appointments, days: updatedDaysCopy } })
+        dispatch({ type: SET_INTERVIEW, value: { appointments } })
         console.log('Updated state successfully')
       })
-  }
-
-  const updateSpots = (appointments) => {
-    return state.days.map(stateDay => {
-      if (stateDay.name !== state.day) {
-        return stateDay
-      }
-
-      const spotsRemaining = stateDay.appointments.filter(e => appointments[e].interview === null).length
-      return {
-        ...stateDay,
-        spots: spotsRemaining
-      }
-    })
   }
 
   const setDay = day => {
