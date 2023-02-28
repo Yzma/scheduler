a2c68aa04097103b0697f139314229b42cebafb1
Yzma <10090913+Yzma@users.noreply.github.com>
2023-02-27 20:51:33 -0500

    Add data-testid to DayListItem and Appointment components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -60,7 +60,7 @@ export default function Appointment(props) {
    }, [transition, mode, props.interview]);
 
   return (
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && props.interview && (<Show student={props.interview.student} interviewer={props.interview.interviewer} onDelete={() => transition(CONFIRM)} onEdit={() => transition(EDIT)} />)}
