a240f5b1542702c803cae48c6525453e9f6b652b
Yzma <10090913+Yzma@users.noreply.github.com>
2023-02-28 15:43:29 -0500

    Add comments for all helper selector functions in selectors.js

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,15 +1,31 @@
 
 
+/**
+ * Returns an array of Appointment objects for the given day,
+ * or returns an empty array if the provided day isn't found.
+ * 
+ * @param {Object} state the State object from useApplicationData
+ * @param {Object} day the day name to retrieve the appointments
+ * @returns A mapped array of Appointments for the provided day
+ */
 export const getAppointmentsForDay = (state, day) => {
-  const foundDay = state.days.find((e) => e.name === day)
+  const foundDay = getDayFromName(state, day)
   if (!foundDay) {
     return []
   }
   return foundDay.appointments.map((e) => state.appointments[e])
 }
 
+/**
+ * Returns an array of interviewers for the given day,
+ * or returns an empty array if the provided day isn't found.
+ * 
+ * @param {Object} state the State object from useApplicationData
+ * @param {Object} day the day name to retrieve the appointments
+ * @returns An array of interviewers that will be available for the day
+ */
 export const getInterviewersForDay = (state, day) => {
-  const foundDay = state.days?.find((e) => e.name === day)
+  const foundDay = getDayFromName(state, day)
   if (!foundDay) {
     return []
   }
@@ -17,47 +33,26 @@ export const getInterviewersForDay = (state, day) => {
   return foundDay.interviewers.map((e) => state.interviewers[e])
 }
 
-// Old version - Done wrong
-// Note: Could use a set to avoid duplicates - Some people might have multiple interviews in the same day
-// export const getInterviewersForDay = (state, day) => {
-// const foundDay = state.days?.find((e) => e.name === day)
-// if(!foundDay) {
-//   return []
-// }
+/**
+ * Searches and returns the day Object from the state Object if the provided name is found,
+ * otherwise returns null.
+ * 
+ * @param {Object} state the State object from useApplicationData
+ * @param {Object} day the day name to retrieve the appointments
+ * @returns The day Object from the days array
+ */
+const getDayFromName = (state, day) => {
+  return state.days?.find((e) => e.name === day)
+}
 
-// const map = foundDay.appointments.flatMap((e) => {
-//   const foundInterviewId = state.appointments[e]?.interview?.interviewer
-//   const foundInterviewer = state.interviewers[foundInterviewId]
-//   if(!foundInterviewer) {
-//     return []
-//   }
-//   return [foundInterviewer]
-// })
-// return Array.from(new Set(map))
-
-// const set = new Set()
-
-// foundDay.appointments.forEach((e) => {
-//   const foundInterviewId = state.appointments[e]?.interview?.interviewer
-//   const foundInterviewer = state.interviewers[foundInterviewId]
-//   if(foundInterviewer) {
-//     set.add(foundInterviewer)
-//   }
-// })
-
-// return Array.from(set)
-
-// const result = []
-// foundDay.appointments.map((e) => {
-//   const foundInterviewId = state.appointments[e]?.interview?.interviewer
-//   const foundInterviewer = state.interviewers[foundInterviewId]
-//   if(foundInterviewer) {
-//     result.push(foundInterviewer)
-//   }
-// })
-// return result
-// }
-
+/**
+ * Constructs and returns a new Interview Object that contains the Interviewer Object (rather than just the interviewer ID)
+ * to be stored in the useApplicationData state Object. Returns null if the provided interviewer is null or isn't found. 
+ * 
+ * @param {Object} state the State object from useApplicationData
+ * @param {Object} day the day name to retrieve the appointments
+ * @returns A new interview Object with the found Interviewers properties 
+ */
 export const getInterview = (state, interview) => {
   const interviewerId = interview?.interviewer
   if (!interviewerId) {
