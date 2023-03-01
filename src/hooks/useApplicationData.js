f27d0561716fda3d4b6f87f2274e38a733977458
Yzma <10090913+Yzma@users.noreply.github.com>
2023-02-28 23:23:43 -0500

    Format hook files

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,12 +1,11 @@
-
-import { useEffect, useReducer } from "react";
-import axios from "axios";
+import { useEffect, useReducer } from "react"
+import axios from "axios"
 
 import reducer, {
   SET_DAY,
   SET_APPLICATION_DATA,
   SET_INTERVIEW
-} from "reducers/application";
+} from "reducers/application"
 
 const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, {
@@ -14,36 +13,23 @@ const useApplicationData = () => {
     days: [],
     appointments: {},
     interviewers: {}
-  });
+  })
 
   useEffect(() => {
-    const socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    const socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL)
 
     socket.onopen = (event) => {
-      socket.send("ping");
-    };
+      socket.send("ping")
+    }
 
     socket.onmessage = (event) => {
       const data = JSON.parse(event.data)
 
-      console.log("Client Message Received:", data);
-
-      if (data.type === 'SET_INTERVIEW') {
-        // {
-        //   type: "SET_INTERVIEW",
-        //   id,
-        //   interview: {
-        //     student,
-        //     interviewer: {
-        //       id
-        //       name,
-        //       avatar
-        //     }
-        //   }
-        // }
-
-        dispatch({ type: SET_INTERVIEW, value: { id: data.id, interview: data.interview } })
-
+      if (data.type === "SET_INTERVIEW") {
+        dispatch({
+          type: SET_INTERVIEW,
+          value: { id: data.id, interview: data.interview }
+        })
       }
     }
 
@@ -52,9 +38,15 @@ const useApplicationData = () => {
       axios.get("/api/appointments"),
       axios.get("/api/interviewers")
     ]).then((all) => {
-      //console.log('All: ', all)
-      dispatch({ type: SET_APPLICATION_DATA, value: { days: all[0].data, appointments: all[1].data, interviewers: all[2].data } })
-    });
+      dispatch({
+        type: SET_APPLICATION_DATA,
+        value: {
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data
+        }
+      })
+    })
 
     return () => {
       socket.close()
@@ -62,30 +54,22 @@ const useApplicationData = () => {
   }, [])
 
   const bookInterview = (id, interview) => {
-
-    //    console.log(id, interview);
-
-    return axios.put(`/api/appointments/${id}`, { id, interview })
-      .then(() => {
-        dispatch({ type: SET_INTERVIEW, value: { id, interview } })
-        //console.log('Updated state successfully')
-      })
+    return axios.put(`/api/appointments/${id}`, { id, interview }).then(() => {
+      dispatch({ type: SET_INTERVIEW, value: { id, interview } })
+    })
   }
 
   const cancelInterview = (id) => {
-
-    return axios.delete(`/api/appointments/${id}`)
-      .then(() => {
-        dispatch({ type: SET_INTERVIEW, value: { id, interview: null } })
-        console.log('Updated state successfully')
-      })
+    return axios.delete(`/api/appointments/${id}`).then(() => {
+      dispatch({ type: SET_INTERVIEW, value: { id, interview: null } })
+    })
   }
 
-  const setDay = day => {
+  const setDay = (day) => {
     dispatch({ type: SET_DAY, value: day })
   }
 
-  return { state, setDay, bookInterview, cancelInterview };
+  return { state, setDay, bookInterview, cancelInterview }
 }
 
 export default useApplicationData
