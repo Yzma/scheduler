55fbc2a1a0b04c3eb2b573f31d78cb8e64f5f9ab
Yzma <10090913+Yzma@users.noreply.github.com>
2023-02-28 23:24:26 -0500

    Format selector class

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,7 @@
-
-
 /**
  * Returns an array of Appointment objects for the given day,
  * or returns an empty array if the provided day isn't found.
- * 
+ *
  * @param {Object} state the State object from useApplicationData
  * @param {Object} day the day name to retrieve the appointments
  * @returns A mapped array of Appointments for the provided day
@@ -19,7 +17,7 @@ export const getAppointmentsForDay = (state, day) => {
 /**
  * Returns an array of interviewers for the given day,
  * or returns an empty array if the provided day isn't found.
- * 
+ *
  * @param {Object} state the State object from useApplicationData
  * @param {Object} day the day name to retrieve the appointments
  * @returns An array of interviewers that will be available for the day
@@ -36,7 +34,7 @@ export const getInterviewersForDay = (state, day) => {
 /**
  * Searches and returns the day Object from the state Object if the provided name is found,
  * otherwise returns null.
- * 
+ *
  * @param {Object} state the State object from useApplicationData
  * @param {Object} day the day name to retrieve the appointments
  * @returns The day Object from the days array
@@ -47,11 +45,11 @@ const getDayFromName = (state, day) => {
 
 /**
  * Constructs and returns a new Interview Object that contains the Interviewer Object (rather than just the interviewer ID)
- * to be stored in the useApplicationData state Object. Returns null if the provided interviewer is null or isn't found. 
- * 
+ * to be stored in the useApplicationData state Object. Returns null if the provided interviewer is null or isn't found.
+ *
  * @param {Object} state the State object from useApplicationData
  * @param {Object} day the day name to retrieve the appointments
- * @returns A new interview Object with the found Interviewers properties 
+ * @returns A new interview Object with the found Interviewers properties
  */
 export const getInterview = (state, interview) => {
   const interviewerId = interview?.interviewer
@@ -59,7 +57,9 @@ export const getInterview = (state, interview) => {
     return null
   }
 
-  const foundInterviewer = Object.values(state.interviewers).find(e => e.id === interviewerId)
+  const foundInterviewer = Object.values(state.interviewers).find(
+    (e) => e.id === interviewerId
+  )
   if (!foundInterviewer) {
     return null
   }
