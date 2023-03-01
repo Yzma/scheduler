9d27eff75bd9efb84cb29c7af4bb6cfb07f961c5
Yzma <10090913+Yzma@users.noreply.github.com>
2023-02-28 23:22:36 -0500

    Format all test files

--- src/reducers/application.test.js
+++ src/reducers/application.test.js
@@ -1,11 +1,9 @@
-import React from "react";
-
-import reducer from "./application";
+import reducer from "./application"
 
 describe("Application", () => {
   it("thows an error with an unsupported type", () => {
     expect(() => reducer({}, { type: null })).toThrowError(
       /tried to reduce with unsupported action type/i
-    );
-  });
+    )
+  })
 })
